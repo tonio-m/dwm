93c229185873b3ababd40c8b92f638fcb8fbcd56
tonio-m <github@mtonio.com>
2022-08-02 16:56:32 -0300

    changing config

--- config.h
+++ config.h
@@ -19,8 +19,8 @@ static const int systraypinningfailfirst =
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
-static const char dmenufont[] = "monospace:size=10";
+static const char *fonts[] = {"monospace:size=12"};
+static const char dmenufont[] = "monospace:size=12";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
@@ -34,7 +34,8 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char *tags[] = {"$", "$2", "🌎",    "🗨", "🔊",
+                             "🎮", "🎬",  "🗀", "🗑"};
 
 static const Rule rules[] = {
     /* xprop(1):
